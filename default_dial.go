5f5b36498d724dffb0100034df5d574e4621e433
zhoujian2 <zhoujian2@sensetime.com>
2021-08-14 11:31:13 +0800

    add more annotations for DefaultDialWithInsecure

--- default_dial.go
+++ default_dial.go
@@ -12,6 +12,7 @@ import (
 // tune them to provide better system's throughput
 const (
 	DialTimeout           = 5 * time.Second
+	MinConnectTimeout     = 3 * time.Second
 	MaxSendMsgSize        = 8 * 1024 * 1024
 	MaxRecvMsgSize        = 8 * 1024 * 1024
 	InitialWindowSize     = 128 * 1024 * 1024
@@ -26,14 +27,23 @@ func DefaultDialWithInsecure(addr string) (*grpc.ClientConn, error) {
 	defer cancel()
 
 	cp := grpc.ConnectParams{
-		Backoff: backoff.DefaultConfig,
+		Backoff:           backoff.DefaultConfig,
+		MinConnectTimeout: MinConnectTimeout,
 	}
-	return grpc.DialContext(ctx, addr, grpc.WithInsecure(),
+	return grpc.DialContext(ctx, addr,
+		// Dial blocks until the underlying TCP connection is up.
+		grpc.WithBlock(),
+		// Dial disables transport security for the underlying TCP connection.
+		grpc.WithInsecure(),
 		grpc.WithDefaultCallOptions(grpc.MaxCallSendMsgSize(MaxSendMsgSize)),
 		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(MaxRecvMsgSize)),
+		// Dial sets the value for initial window size on a stream.
 		grpc.WithInitialWindowSize(InitialWindowSize),
+		// Dial sets the value for initial window size on on a TCP connection.
 		grpc.WithInitialConnWindowSize(InitialConnWindowSize),
+		// Dial specifies the options for connection backoff.
 		grpc.WithConnectParams(cp),
+		// Dial specifies keepalive parameters for the client transport.
 		grpc.WithKeepaliveParams(keepalive.ClientParameters{
 			Time:                KeepAliveTime,
 			Timeout:             KeepAliveTimeout,
